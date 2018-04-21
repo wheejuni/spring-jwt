6581fbc2257e958bcd44a71d1ca09dca4329e77b
seulgiwendy <wheejuni@gmail.com>
2018-04-21 20:58:45 +0900

    [data-modeling]Repository 작성

---
+++ src/main/java/com/wheejuni/jwtdemo/domain/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.wheejuni.jwtdemo.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends CrudRepository<Account, Long> {
+
+    Optional<Account> findByUserId(String userId);
+
+    Optional<Account> findBySocialId(long socialId);
+
+}
