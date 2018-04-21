42adaad7d935ebd80a232222970cf6bf619c5311
seulgiwendy <wheejuni@gmail.com>
2018-04-21 20:36:57 +0900

    [project]기본 코딩 전 설정

--- src/main/java/com/wheejuni/jwtdemo/controllers/ApiController.java
+++ src/main/java/com/wheejuni/jwtdemo/controllers/ApiController.java
@@ -1,5 +1,6 @@
 package com.wheejuni.jwtdemo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ApiController {
 
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
 
 }
