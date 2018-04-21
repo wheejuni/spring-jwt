fa086fba2fb5858986fa8d244a985e12bb33e783
seulgiwendy <wheejuni@gmail.com>
2018-04-21 17:53:47 +0900

    [project]HTTP 테스트를 위한 리퀘스트 파일 작성

---
+++ src/main/java/com/wheejuni/jwtdemo/controllers/ApiController.java
@@ -0,0 +1,11 @@
+package com.wheejuni.jwtdemo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+
+}
--- src/main/java/com/wheejuni/jwtdemo/controllers/ApiController.kt
+++
@@ -1,2 +0,0 @@
-package com.wheejuni.jwtdemo.controllers
-
