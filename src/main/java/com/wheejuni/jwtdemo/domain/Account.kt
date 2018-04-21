e42e05ee9dfecf976ecc9179ee86336ceeeab9f5
seulgiwendy <wheejuni@gmail.com>
2018-04-21 20:55:07 +0900

    [data-modeling]사용자 객체 작성

--- src/main/java/com/wheejuni/jwtdemo/domain/Account.kt
+++ src/main/java/com/wheejuni/jwtdemo/domain/Account.kt
@@ -1,20 +1,30 @@
 package com.wheejuni.jwtdemo.domain
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
+@Table(name = "ACCOUNT")
 internal data class Account(
 
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
-        private val id: Long? = null) {
+        private val id: Long? = null,
 
-    companion object {
-        //TODO(Write Logic for dto -> model)
-    }
+        @Column(name = "ACCOUNT_USERNAME")
+        private val username: String? = null,
 
+        @Column(name = "ACCOUNT_LOGINID")
+        private val userId: String? = null,
 
-}
+        @Column(name = "ACCOUNT_PASSWORD")
+        private val password: String? = null,
+
+        @Column(name = "ACCOUNT_ROLE")
+        @Enumerated(value = EnumType.STRING)
+        private val userRole: UserRole? = null,
+
+        @Column(name = "ACCOUNT_SOCIAL_ID")
+        private val socialId: Long? = null,
+
+        @Column(name = "ACCOUNT_SOCIAL_PROFILEPIC")
+        private val profileHref: String? = null)
